fc61f04ebf37c450cff848a330dc1f3959a23419
Sarah Newman <sanewman04@gmail.com>
2026-04-13 05:26:55 +0000

    added resources and recipe pages

--- utilities.js
+++ utilities.js
@@ -2,11 +2,22 @@ const adultMode = document.getElementById('adult-mode');
 const kidsMode = document.getElementById('kids-mode');
 const buttons = document.querySelectorAll('nav button');
 
+const savedMode = localStorage.getItem('mode') || 'adult';
+if (savedMode === 'kids') {
+    adultMode.classList.add('hidden');
+    kidsMode.classList.remove('hidden');
+} else {
+    kidsMode.classList.add('hidden');
+    adultMode.classList.remove('hidden');
+}
+
 buttons[0].addEventListener('click', () => {
+    localStorage.setItem('mode', 'adult');
     adultMode.classList.remove('hidden');
     kidsMode.classList.add('hidden');
 });
 buttons[1].addEventListener('click', () => {
+    localStorage.setItem('mode', 'kids');
     kidsMode.classList.remove('hidden');
     adultMode.classList.add('hidden');
 });
